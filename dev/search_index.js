ee2d21905d8537acdd9834a0f2f2d0e84a7764fd
Documenter.jl <documenter@juliadocs.github.io>
2024-02-28 07:58:18 +0000

    build based on ff5b9f9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InvariantManifolds","category":"page"},{"location":"#InvariantManifolds","page":"Home","title":"InvariantManifolds","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InvariantManifolds.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InvariantManifolds]","category":"page"}]
+}
