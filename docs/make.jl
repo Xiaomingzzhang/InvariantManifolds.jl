e8185b0ec3bb9a39b1aa8a498c0f2b546d80e983
xiaoming <13276587+mingwebsite@user.noreply.gitee.com>
2024-02-28 18:48:15 +0800

    update

--- docs/make.jl
+++ docs/make.jl
@@ -22,6 +22,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/Xiaomingzzhang/InvariantManifolds.jl.git",
-    devbranch="main",
+    repo="github.com/Xiaomingzzhang/InvariantManifolds.jl",
+    devbranch="gh-pages",
 )
