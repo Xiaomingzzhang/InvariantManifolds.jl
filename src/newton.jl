18395fddee44276e194a610d79656b2af017296c
Xiaomingzzhang <xiaomingzzhang@163.com>
2025-03-13 19:32:45 +0800

    add finitediff method to nonsmooth system

--- src/newton.jl
+++ src/newton.jl
@@ -43,10 +43,16 @@ end
 - `n` maximum iterate times, default to be 100;
 - `abstol` absolute tolerance for the fixed point, default to be `1e-8`;
 - `alg` the algorithm used to solve the ODE, default to be `Vern9()`.
-# Returns
-A [`Saddle`](@ref) object, which contains the fixed point and the directions of the stable manifold.
+We also provide the finite difference method to find the saddle of the time-T-map of nonsmooth ODE systems:
+# Parameters
+- `setup` the [`NSState`](@ref) of the nonsmooth ODE system;
+- `x` the initial point to iterate.
+- `p` the parameters of the nonsmooth ODE system.
+# Keyword arguments
+- `n` maximum iterate times, default to be 100;
+- `abstol` absolute tolerance for the fixed point, default to be `1e-8`;
 """
-function findsaddle(v, dv, timespan, x::SVector{N,T}, p; n=100, abstol=1e-8, alg=Vern9()) where{N,T}
+function findsaddle(v, dv, timespan, x::SVector{N,T}, p; n=100, abstol=1e-8, alg=Vern9()) where {N,T}
     timemap = ODESolver(v, timespan, alg, abstol)
     ii = SMatrix{N,N,T}(I)
     function jac(u, p)
@@ -62,6 +68,33 @@ function findsaddle(v, dv, timespan, x::SVector{N,T}, p; n=100, abstol=1e-8, alg
         data[1] = data[2]
         data[2] = data[1] - inv(jac(data[1], p) - ii) * (timemap(data[1], p)[end] - data[1])
         i += 1
+    end
+    if norm(data[2] - data[1]) < abstol
+        eigendata = eigen(jac(data[2], p))
+        indics = findall(>(1), abs.(eigendata.values))
+        directions = [eigendata.vectors[:, i] for i in indics]
+        println("Fixed point found successfully:")
+        Saddle(data[2], directions, [eigendata.values[i] for i in indics])
+    else
+        println("Failed to find a fixed point after $n times iterations. The last point is:")
+        Saddle(data[2], typeof(data[2])[], T[])
+    end
+end
+
+
+function findsaddle(setup::NSSetUp, x::SVector{N,T}, p; n=100, abstol=1e-8) where{N,T}
+    timemap = setup.timetmap
+    ii = SMatrix{N,N,T}(I)
+    function jac(u, p)
+        ff(x) = timemap(NSState(x), p).state
+        FiniteDiff.finite_difference_jacobian(ff, u)
+    end
+    xn = x - inv(jac(x, p) - ii) * (timemap(NSState(x), p).state - x)
+    data = [x, xn]
+    i = 1
+    while norm(data[2] - data[1]) > abstol && i <= n
+        data[1] = data[2]
+        data[2] = data[1] - inv(jac(data[1], p) - ii) * (timemap(NSState(data[1]), p).state - data[1])
     end
     if norm(data[2] - data[1]) < abstol
         eigendata = eigen(jac(data[2], p))
